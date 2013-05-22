d770e5ef2c1bef810b4408fa51cb4cd467aa7156
Eloy Gomez <eloy@indeos.es>
2013-05-22 20:24:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sindicato::Application.config.secret_token = 'cf3cb5dde75fc3caef1f8e00fcdef36a7f3e6ca64c64ffe09a744334052c3bbb9f6600adc4c04555b656c165a22def809252b16647d112b8e330d8d3cb491d1a'
